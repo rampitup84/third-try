864782eda160ccf7464c0e3c748d158bf002503e
Josesito <ctzkne@gmail.com>
2018-08-21 17:17:12 -0500

    Done with the animation video.

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,13 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Learn to <br />design and code React apps</h1>
-<p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
-    
-    <Link to="/page-2/">Watch the video now... or not.</Link>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Learn to <br />design and code React apps</h1>
+        <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
+        <a href="/page-2/">Watch the video</a>
+      </div>
+    </div>
   </div>
 )
 
